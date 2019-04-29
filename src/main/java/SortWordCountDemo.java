50890837d6ee69fdb7144a74e0cf0baca5efed3d
李艳杰 <liyanjie@belle.com.cn>
2019-04-29 13:52:58 +0800

    add annotation

--- src/main/java/SortWordCountDemo.java
+++ src/main/java/SortWordCountDemo.java
@@ -2,18 +2,16 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.FlatMapFunction;
-import org.apache.spark.api.java.function.Function;
-import org.apache.spark.api.java.function.Function2;
-import org.apache.spark.api.java.function.PairFunction;
-import scala.Tuple1;
+import org.apache.spark.api.java.function.*;
 import scala.Tuple2;
 
 import java.util.Arrays;
 import java.util.Iterator;
-import java.util.List;
 
-public class Persisent {
+/**
+ * wordCount 程序 java版本
+ */
+public class SortWordCountDemo {
     public <T> T show(T t) {
         return t;
     }
@@ -21,8 +19,10 @@ public class Persisent {
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setMaster("local").setAppName("hello");
         JavaSparkContext sc = new JavaSparkContext(conf);
+        //读取目标文件
         JavaRDD<String> fileRdd = sc.textFile("E:/686/personal/Demo.java");
         System.out.println("文件中取出共：" + fileRdd.count());
+        //压平
         JavaRDD<String> flatRes = fileRdd.flatMap(new FlatMapFunction<String, String>() {
             @Override
             public Iterator<String> call(String line) throws Exception {
@@ -30,6 +30,7 @@ public class Persisent {
             }
         });
         System.out.println("压平后共：" + flatRes.count());
+        //转换为tuple
         JavaPairRDD<String, Integer> mapPairRes = flatRes.mapToPair(new PairFunction<String, String, Integer>() {
             @Override
             public Tuple2<String, Integer> call(String word) throws Exception {
@@ -37,7 +38,7 @@ public class Persisent {
             }
         });
         System.out.println("tuple后共：" + mapPairRes.count());
-
+        //按照key聚合
         JavaPairRDD<String, Integer> wordCountRes = mapPairRes.reduceByKey(new Function2<Integer, Integer, Integer>() {
             @Override
             public Integer call(Integer v1, Integer v2) throws Exception {
@@ -46,7 +47,7 @@ public class Persisent {
         });
 
         System.out.println("统计结果为:" + wordCountRes.count());
-
+        //交换key-value位置
         JavaPairRDD<Integer, String> wcResRevers = wordCountRes.mapToPair(new PairFunction<Tuple2<String, Integer>, Integer, String>() {
             @Override
             public Tuple2<Integer, String> call(Tuple2<String, Integer> wcRes) throws Exception {
@@ -54,14 +55,22 @@ public class Persisent {
                 return new Tuple2<Integer, String>(wcRes._2, wcRes._1);
             }
         });
+        //按照出现次数排序
         JavaPairRDD<Integer, String> sortRes = wcResRevers.sortByKey(false);
-        JavaRDD<Tuple2> map = sortRes.map(new Function<Tuple2<Integer, String>, Tuple2>() {
+        //互换key-value位置
+        JavaRDD<Tuple2> res = sortRes.map(new Function<Tuple2<Integer, String>, Tuple2>() {
             @Override
             public Tuple2 call(Tuple2<Integer, String> v1) throws Exception {
                 return new Tuple2<String, Integer>(v1._2, v1._1);
             }
         });
-        System.out.println(sortRes);
+        //打印
+        res.foreach(new VoidFunction<Tuple2>() {
+            @Override
+            public void call(Tuple2 t) throws Exception {
+                System.out.println(t._1.toString().trim() + " appear " + t._2 + " times .");
+            }
+        });
 
     }
 }
--- src/main/scala/cn/wonhigh/test/SortWordCountDemo.scala
+++ src/main/scala/cn/wonhigh/test/SortWordCountDemo.scala
@@ -3,17 +3,29 @@ package cn.wonhigh.test
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
-object SortWordCount {
+/**
+  * wordCount 程序 scala版本
+  *
+  */
+object SortWordCountDemo {
   def main(args: Array[String]): Unit = {
     val conf: SparkConf = new SparkConf().setAppName("test").setMaster("local")
     val sc = new SparkContext(conf)
+    //读取目标文件
     val lineRdd: RDD[String] = sc.textFile("E:/686/personal/Demo.java", 1)
+    //压平
     val wordsRdd: RDD[String] = lineRdd.flatMap(line => line.split(" "))
+    //转换为tuple
     val wordValRdd: RDD[(String, Int)] = wordsRdd.map(word => (word, 1))
+    //按照key聚合
     val reduceRes: RDD[(String, Int)] = wordValRdd.reduceByKey(_ + _)
+    //交换key-value位置
     val reverseRes: RDD[(Int, String)] = reduceRes.map(t => (t._2, t._1))
+    //按照出现次数排序
     val sortRes: RDD[(Int, String)] = reverseRes.sortByKey(false)
+    //互换key-value位置
     val res: RDD[(String, Int)] = sortRes.map(t => (t._2, t._1))
-    println(res)
+    //打印
+    res.foreach(t => println(t._1.trim + " appear " + t._2 + " times ."))
   }
 }
